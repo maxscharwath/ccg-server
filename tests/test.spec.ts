8f67052aba0c30d7bddadde85c04c8d38025a61f
Maxime Scharwath <maxscharwath@gmail.com>
2021-10-22 10:40:39 +0200

    add jest tests workflows

---
+++ tests/test.spec.ts
@@ -0,0 +1,8 @@
+describe("Test jest", () => {
+    test("it should be true", () => {
+        expect(true).toBe(true);
+    });
+    test("it should be false", () => {
+        expect(false).toBe(false);
+    });
+});
